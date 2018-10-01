38f7b7a08c24282e4fcf5fbdc48fd449b5a287cb
Sui <suikunguan@gmail.com>
2018-10-01 12:52:25 -0700

    correct q10. check errata

--- hw4/q10.py
+++ hw4/q10.py
@@ -2,7 +2,7 @@ import numpy as np
 import math
 
 #mean matrix u from (4.8)
-u = np.array([7/4, 7/4, 5/4, 2, 2, 1])
+u = np.array([7.0/4, 7.0/4, 5.0/4, 2, 2, 1])
 
 #u1, u2, u3 from (4.10)
 u1 = np.array([0.1641, 0.6278, -0.2604, -0.5389, 0.4637, 0.0752])
@@ -17,13 +17,20 @@ def score(Y):
    yHat = Y - u
    #only uses two most significant u1 and u2 
    w = np.array([np.dot(yHat, u1), np.dot(yHat, u2)])
-   print(w)
-   #score the min distance with each column in the score matrix
+   #score: min distance with each column in the score matrix
    scores = []
    for column in range(scoreMat.shape[1]):
       distance = math.sqrt(np.sum((w - scoreMat[:,column])**2))
       scores.append(distance)
    return min(scores)
 
-Y = np.array([2, 3, 4, 4, -3, -2])
-print(score(Y))
+Y1 = np.array([2, 3, 1, 0, 3, 2])
+Y2 = np.array([-4, -5, 0, 3, 1, -2])
+Y3 = np.array([2, 3, 0, 1, 3, 2])
+Y4 = np.array([3, 2, 1, 0, 3, 2])
+
+print("score(Y1) = %.2f" % score(Y1))
+print("score(Y2) = %.2f" % score(Y2))
+print("score(Y3) = %.2f" % score(Y3))
+print("score(Y4) = %.2f" % score(Y4))
+
