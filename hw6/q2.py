23d80b25b91c866c626f39c68169aa587b878d77
Sui <suikunguan@gmail.com>
2018-11-05 18:10:09 -0800

    Added graph

--- hw6/q2.py
+++ hw6/q2.py
@@ -1,3 +1,6 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
 xorTruth = {
    (0,0):0,
    (0,1):1,
@@ -14,35 +17,6 @@ def compare(w0, w1, w2, w3, w4, w5):
          return False 
    return True
 
-def compute(w0,w1,w2,w3,w4,w5):
-   x0 = 0
-   x1 = 0
-   print("input (%s, %s) , y = %s" % (x0, x1, y(w0,w1,w2,w3,w4,w5,x0,x1)))
-   x0 = 0
-   x1 = 0.5
-   print("input (%s, %s) , y = %s" % (x0, x1, y(w0,w1,w2,w3,w4,w5,x0,x1)))
-   x0 = 0
-   x1 = 1
-   print("input (%s, %s) , y = %s" % (x0, x1, y(w0,w1,w2,w3,w4,w5,x0,x1)))
-   x0 = 0.5
-   x1 = 0
-   print("input (%s, %s) , y = %s" % (x0, x1, y(w0,w1,w2,w3,w4,w5,x0,x1)))
-   x0 = 0.5
-   x1 = 0.5
-   print("input (%s, %s) , y = %s" % (x0, x1, y(w0,w1,w2,w3,w4,w5,x0,x1)))
-   x0 = 0.5
-   x1 = 1
-   print("input (%s, %s) , y = %s" % (x0, x1, y(w0,w1,w2,w3,w4,w5,x0,x1)))
-   x0 = 1
-   x1 = 0
-   print("input (%s, %s) , y = %s" % (x0, x1, y(w0,w1,w2,w3,w4,w5,x0,x1)))
-   x0 = 1
-   x1 = 0.5
-   print("input (%s, %s) , y = %s" % (x0, x1, y(w0,w1,w2,w3,w4,w5,x0,x1)))
-   x0 = 1
-   x1 = 1
-   print("input (%s, %s) , y = %s" % (x0, x1, y(w0,w1,w2,w3,w4,w5,x0,x1)))
-
 weights = [-1, 1]
 for w0 in weights:
    for w1 in weights:
@@ -52,4 +26,25 @@ for w0 in weights:
                for w5 in weights:
                   if compare(w0,w1,w2,w3,w4,w5):
                      print("XOR: w0=%d,w1=%d,w2=%d,w3=%d,w4=%d,w5=%d" % (w0,w1,w2,w3,w4,w5))
-                     compute(w0,w1,w2,w3,w4,w5)
+
+
+#plot a solution
+w0,w1,w2,w3,w4,w5 = (-1,1,1,-1,1,1)
+n = 100
+x0 = np.linspace(0, 1, n)
+x1 = np.linspace(0, 1, n)
+red = (1, 0, 0)
+blue = (0, 0, 1)
+c = np.zeros((n*n, 3))
+x0p = np.zeros((n*n,))
+x1p = np.zeros((n*n,))
+for i in range(n):
+   for j in range(n):
+      if y(w0,w1,w2,w3,w4,w5,x0[i],x1[j]) >= 0.5: c[i*n+j]=red 
+      else: c[i*n+j] = blue
+      x0p[i*n+j] = x0[i]
+      x1p[i*n+j] = x1[j]
+print("plotting solution for w0,w1,w2,w3,w4,w5=%s,%s,%s,%s,%s,%s" % (w0,w1,w2,w3,w4,w5))
+print("graph: red = 1, blue = 0")
+plt.scatter(x0p, x1p, c=c)
+plt.show()
