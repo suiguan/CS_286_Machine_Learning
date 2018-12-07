55c36af494bce192ec47c0cf8fb65af3fc8092ad
Sui <suikunguan@gmail.com>
2018-12-07 15:59:16 -0800

    change c7q1 with scikit-learn API

--- hw7/c7q1.py
+++ hw7/c7q1.py
@@ -1,91 +1,54 @@
-#chapter 7 Q1
+#Chapter 7, Q1
+#Reference: https://scikit-learn.org/stable/auto_examples/neighbors/plot_classification.html
+#on 12/7/2018
 
 import numpy as np
 import matplotlib.pyplot as plt
-import math
+from matplotlib.colors import ListedColormap
+from sklearn import neighbors, datasets
 
-red = (1, 0, 0)
-blue = (0, 0, 1)
-green = (0, 1, 0)
 
-#data points and colors from Ch-7 Q1
+#hardcoded points in Ch7, Q1
 points = [(0.5, 3.00), (1.0, 4.25), (1.5, 2.00), (2.0, 2.75), (2.5, 1.65),
          (3.0, 2.70), (3.5, 1.00), (4.0, 2.50), (4.5, 2.10), (5.0, 2.75),
          (0.5, 1.75), (1.5, 1.50), (2.5, 4.00), (2.5, 2.10), (3.0, 1.50),
          (3.5, 1.85), (4.0, 3.50), (5.0, 1.45),]
+X = np.array(points)
 
-colors = [red, red, red, red, red, red, red, red, red, red, 
-         blue, blue, blue, blue, blue, blue, blue, blue,]
-
-#plot each points with their colors
-xs = np.array([p[0] for p in points])
-ys = np.array([p[1] for p in points])
-plt.figure(0)
-plt.scatter(xs, ys, c=colors)
+colors = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+         1, 1, 1, 1, 1, 1, 1, 1,]
+y = np.array(colors)
 
 
-def distance(p1, p2):
-   return math.sqrt(math.pow(p1[0] - p2[0], 2) + math.pow(p1[1] - p2[1], 2))
+n_neighbors = 3
+h = .02  # step size in the mesh
 
-def kNearestNeighbors(p0, points, k):
-   nearest = []
-   for _k in range(k): nearest.append(None)
+# Create color maps
+cmap_light = ListedColormap(['#FFAAAA', '#AAAAFF'])
+cmap_bold = ListedColormap(['#FF0000', '#0000FF'])
 
-   for pidx, p in enumerate(points):
-      d = distance(p0, p)
-      for i in range(k):
-         n = nearest[i]
-         if n == None: 
-            nearest[i] = (pidx, d) 
-            break
-         else:
-            _, _d = n
-            if d < _d: 
-               nearest[i] = (pidx, d)
-               d = _d #changes the distance to the one that has pop out from the list
-                      #if there this one is part of the nearest, it will replace with later entries 
-   
-   idx = None
-   for i, n in enumerate(nearest): 
-      if n == None: 
-         idx = i 
-         break
+for weights in ['distance']:
+    # we create an instance of Neighbours Classifier and fit the data.
+    clf = neighbors.KNeighborsClassifier(n_neighbors, weights=weights)
+    clf.fit(X, y)
 
-   if idx != None: nearest = nearest[:idx]
-   
-   #create counter to find the majority
-   counter = {}
-   for idx, _ in nearest:
-      c = colors[idx]
-      if not c in counter: counter[c] = 0
-      counter[c] += 1
+    # Plot the decision boundary. For that, we will assign a color to each
+    # point in the mesh [x_min, x_max]x[y_min, y_max].
+    x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                         np.arange(y_min, y_max, h))
+    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
 
-   maxCount = None
-   majorityC = None
-   for c in counter.keys():
-      if maxCount == None: 
-         maxCount = counter[c] 
-         majorityC = c
-      elif counter[c] > maxCount: 
-         maxCount = counter[c]
-         majorityC = c
-   if maxCount == None or maxCount < k/2: print("maxCount %s error. k = %d" % (maxCount, k))
-   return majorityC
+    # Put the result into a color plot
+    Z = Z.reshape(xx.shape)
+    plt.figure()
+    plt.pcolormesh(xx, yy, Z, cmap=cmap_light)
 
-n = 100
-x0 = np.linspace(0, 6, n)
-x1 = np.linspace(0, 6, n)
-c = np.zeros((n*n, 3))
-x0p = np.zeros((n*n,))
-x1p = np.zeros((n*n,))
-for i in range(n):
-   for j in range(n):
-      c[i*n+j] = kNearestNeighbors((x0[i], x1[j]), points, 3)
-      x0p[i*n+j] = x0[i]
-      x1p[i*n+j] = x1[j] 
+    # Plot also the training points
+    plt.scatter(X[:, 0], X[:, 1], c=y, cmap=cmap_bold, edgecolor='k', s=20)
+    plt.xlim(xx.min(), xx.max())
+    plt.ylim(yy.min(), yy.max())
+    plt.title("2-Class classification (k = %i, weights = '%s')" % (n_neighbors, weights))
 
-plt.figure(1)
-plt.scatter(x0p, x1p, c=c)
-
-#display graphs
 plt.show()
